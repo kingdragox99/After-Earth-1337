1335bd8a9ebf2018a0fcaf6cc03347ff0f9b6adf
Arthur Lefevre <kingdragox99@gmail.com>
2022-12-28 02:28:59 +0100

    feat: prototype map

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  purge: ["./public/index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  darkMode: false,
+  content: ["./public/**/*.html", "./src/**/*.{js,jsx,ts,tsx,vue}"],
   theme: {
     extend: {},
   },
